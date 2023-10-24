ba3875776505f5799881c97b24bb92aedf656af9
Vladas <146100@gmail.com>
2023-10-24 21:11:04 +0300

    added call user method

--- src/integration_contract.rs
+++ src/integration_contract.rs
@@ -3,7 +3,7 @@ use std::mem::{size_of, MaybeUninit};
 use anyhow::Result;
 use async_trait::async_trait;
 use near_sdk::serde::{de::DeserializeOwned, Serialize};
-use workspaces::{Account, Contract};
+use workspaces::{operations::CallTransaction, Account, Contract};
 
 #[async_trait]
 pub trait IntegrationContract<'a> {
@@ -14,23 +14,26 @@ pub trait IntegrationContract<'a> {
 
     async fn call_contract<T: DeserializeOwned, P: Serialize + Send>(&self, method: &str, args: P) -> Result<T> {
         println!("▶️ {method}");
+        invoke_transaction(self.contract().call(method), args).await
+    }
 
-        let result = self
-            .contract()
-            .call(method)
-            .args_json(args)
-            .max_gas()
-            .transact()
-            .await?
-            .into_result()?;
+    async fn call_user<T: DeserializeOwned, P: Serialize + Send>(&self, method: &str, args: P) -> Result<T> {
+        println!("▶️ {method}");
+        invoke_transaction(self.user_account().call(self.contract().id(), method), args).await
+    }
+}
 
-        if size_of::<T>() == 0 {
-            // For cases when return type is `()` and we don't need to parse result.
-            // This call is safe for zero sized types.
-            #[allow(clippy::uninit_assumed_init)]
-            Ok(unsafe { MaybeUninit::uninit().assume_init() })
-        } else {
-            Ok(result.json()?)
-        }
+async fn invoke_transaction<T: DeserializeOwned, P: Serialize + Send>(tx: CallTransaction<'_>, args: P) -> Result<T> {
+    let result = tx.args_json(args).max_gas().transact().await?.into_result()?;
+
+    println!("Result: {:?}", result);
+
+    if size_of::<T>() == 0 {
+        // For cases when return type is `()` and we don't need to parse result.
+        // This call is safe for zero sized types.
+        #[allow(clippy::uninit_assumed_init)]
+        Ok(unsafe { MaybeUninit::uninit().assume_init() })
+    } else {
+        Ok(result.json()?)
     }
 }
